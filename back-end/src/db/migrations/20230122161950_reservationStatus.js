306ab454f0c1c0381a960eaf7e5dc6a9383453a4
bpeskett <bwptwo@gmail.com>
2023-01-22 17:50:52 -0500

    reservationStatus

--- back-end/src/db/migrations/20230122161950_reservationStatus.js
+++ back-end/src/db/migrations/20230122161950_reservationStatus.js
@@ -1,5 +1,5 @@
 exports.up = function (knex) {
-    return knex.schema.alterTable("reservations", (table) => {
+    return knex.schema.table("reservations", (table) => {
       table.string("status").defaultTo("booked").notNullable().index();
     });
   };
