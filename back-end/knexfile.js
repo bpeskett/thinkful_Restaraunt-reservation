85c407bfd65edf1152881e0e339668469c360a8d
bpeskett <bwptwo@gmail.com>
2023-01-26 20:45:56 -0500

    url update

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://honzthlc:g7C-NWrp-JQOMy4auo4E5mKnyyOA_zaz@salt.db.elephantsql.com/honzthlc",
+  DATABASE_URL_DEVELOPMENT = "postgres://yzgzcaos:eafrOTIklGC-osJCBgevvURLBemwBN7O@salt.db.elephantsql.com/yzgzcaos",
+  DATABASE_URL_TEST = "postgres://pfzsjeyh:rKlomAZhexTfJRzagcan2Otumu8uKsdy@salt.db.elephantsql.com/pfzsjeyh",
+  DATABASE_URL_PREVIEW = "postgres://yyitgmaa:Osa2XaD8SjIY9tziZbV-wBi2ThkPTL4F@salt.db.elephantsql.com/yyitgmaa",
   DEBUG,
 } = process.env;
 
